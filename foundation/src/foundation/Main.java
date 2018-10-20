edb58895ae147b072f8cc4de08de5fc19da48e89
satvirsingh <satvirsingh@satvirs-air.hsd1.or.comcast.net>
2018-10-20 02:49:31 -0700

    second line

--- foundation/src/foundation/Main.java
+++ foundation/src/foundation/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) { 
 		System.out.println("Tutoring Project's first line of code");  
-		
+		System.out.println("Second line of code");
 	}
 
 }
