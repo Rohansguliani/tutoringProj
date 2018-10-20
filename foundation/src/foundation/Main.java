cc7969f94bee427aeca78a8902c3a5d7d3b03ca2
Rohan Guliani <rohan.guliani285460@gmail.com>
2018-10-20 01:40:49 -0700

    first commit

--- foundation/src/foundation/Main.java
+++ foundation/src/foundation/Main.java
@@ -3,7 +3,7 @@ package foundation;
 public class Main {
 	
 	public static void main(String[] args) { 
-		System.out.println("Tutoring Project's first line of code"); 
+		System.out.println("Tutoring Project's first line of code");  
 		
 	}
 
